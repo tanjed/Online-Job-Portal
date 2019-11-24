06bb5244b143f2579eaa55173ee67c93dd6750c8
tanjed <ahmed15-6849@diu.edu.bd>
2019-11-24 22:15:33 +0600

    Header Updated

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,8 +1,24 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <!-- Brand/logo -->
-    <a class="navbar-brand" href="{{route('company.dashboard.show')}}">
-        Online Job Portal
-    </a>
+
+    @auth('applicant')
+        <!-- Brand/logo -->
+        <a class="navbar-brand" href="{{route('applicant.dashboard.show')}}">
+            Online Job Portal
+        </a>
+    @endauth
+
+    @auth('company')
+        <!-- Brand/logo -->
+            <a class="navbar-brand" href="{{route('company.dashboard.show')}}">
+                Online Job Portal
+            </a>
+    @endauth
+
+    @guest
+        <a class="navbar-brand" href="{{route('applicant.dashboard.show')}}">
+            Online Job Portal
+        </a>
+    @endguest
 
     <!-- Links -->
     <ul class="navbar-nav" style="width: 100%;">
@@ -11,11 +27,16 @@
                 <a class="nav-link text-light">{{auth('applicant')->user()->first_name}}</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-light" href="{{URL::to(route('company.logout'))}}" >Logout</a>
+                <a class="nav-link text-light" href="{{URL::to(route('applicant.logout'))}}" >Logout</a>
             </li>
-            @else
+        @endauth
+
+        @auth('company')
             <li class="nav-item" style="margin-left: 80%">
-                <a class="nav-link text-light disabled">Hello</a>
+                <a class="nav-link text-light">{{auth('company')->user()->first_name}}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="{{URL::to(route('company.logout'))}}" >Logout</a>
             </li>
         @endauth
     </ul>
